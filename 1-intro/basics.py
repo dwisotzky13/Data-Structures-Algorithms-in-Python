41a931bafd6ab464bbaa31e84a58f0b8f79d4f1b
dwisotzky13 <75093284+dwisotzky13@users.noreply.github.com>
2021-08-02 19:28:46 -0400

    Create basics.py

---
+++ 1-intro/basics.py
@@ -0,0 +1,7 @@
+def show_excitement():
+    # Your code goes here!
+    here = ''
+    for i in range(5):
+        here += "I am super excited for this course!\n"
+    return here[:-1]
+print(show_excitement())
